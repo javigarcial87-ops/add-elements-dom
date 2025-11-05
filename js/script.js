306f5fc70b86527b47168aff5dce9ecd5ad3160e
javier garcia alvarez <javigarcial87@gmail.com>
2025-11-05 15:34:10 +0100

    refactor button event handling and simplify agregarElemento function in script.js

--- js/script.js
+++ js/script.js
@@ -1,21 +1,35 @@
  // Aquí tu código
-const agregar = document.getElementById("agregarElemento");
-const list = document.getElementById("lista");
-const nuevoElemento = prompt("Introduce un elemento para agregar a la lista:");
 
-agregar.addEventListener("click", () => {
+function agregarElemento(){
+  const nuevoElemento=prompt("Introduce un nuevo elemento");
+  if(nuevoElemento && nuevoElemento.trim()!==""){
+    const lista = document.getElementById("lista")
+    const li=document.createElement("li");
+    li.textContent=nuevoElemento;
+    lista.appendChild(li);
+  } else{
+    alert("Por favor, escribe un texto válido antes de agregarlo.");
+  }
+}
+
+
+// const agregar = document.getElementById("agregarElemento");
+// const list = document.getElementById("lista");
+// const nuevoElemento = prompt("Introduce un elemento para agregar a la lista:");
+
+// agregar.addEventListener("click", () => {
  
  
 
   
-  if (nuevoElemento && nuevoElemento.trim() !== "") {
+//   if (nuevoElemento && nuevoElemento.trim() !== "") {
     
-    const li = document.createElement("li");
-    li.textContent = nuevoElemento;
+//     const li = document.createElement("li");
+//     li.textContent = nuevoElemento;
 
     
-    list.appendChild(li);
-  } else {
-    alert("Por favor, escribe un texto válido antes de agregarlo.");
-  }
-});
+//     list.appendChild(li);
+//   } else {
+//     alert("Por favor, escribe un texto válido antes de agregarlo.");
+//   }
+// });
