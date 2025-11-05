3bc7ce145171cbc0efc7a0faaa47a534e997880d
javier garcia alvarez <javigarcial87@gmail.com>
2025-11-05 20:35:42 +0100

    remove commented-out agregarElemento function for cleaner script.js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,30 @@
 
 
-function agregarElemento(){
-  const nuevoElemento=prompt("Introduce un nuevo elemento");
-  if(nuevoElemento && nuevoElemento.trim()!==""){
-    const lista = document.getElementById("lista")
-    const li=document.createElement("li");
-    li.textContent=nuevoElemento;
-    lista.appendChild(li);
-  } else{
-    alert("Por favor, escribe un texto válido antes de agregarlo.");
-  }
-}
+// function agregarElemento(){
+//   const nuevoElemento=prompt("Introduce un nuevo elemento");
+//   if(nuevoElemento && nuevoElemento.trim()!==""){
+//     const lista = document.getElementById("lista")
+//     const li=document.createElement("li");
+//     li.textContent=nuevoElemento;
+//     lista.appendChild(li);
+//   } else{
+//     alert("Por favor, escribe un texto válido antes de agregarlo.");
+//   }
+// }
+
+const lista = document.getElementById('lista');
+const agregar = document.getElementById('agregar');
+agregar.addEventListener('click', function(){
+const producto = prompt('Añade un producto');
+const li = document.createElement('li');
+li.textContent = producto;
+lista.appendChild(li)  
+});
+
+
+
+
+
+
 
 
